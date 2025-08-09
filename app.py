ee80652d7ce3bf090635ff1450f156903b9adf5d
Sinomar Júnior <138079956+SinomarJrs@users.noreply.github.com>
2025-08-08 22:57:12 -0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
@@ -135,4 +134,5 @@ with col_graf4:
 
 # --- Tabela de Dados Detalhados ---
 st.subheader("Dados Detalhados")
+
 st.dataframe(df_filtrado)
